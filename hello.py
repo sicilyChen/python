3a2192dd664e59d1d4b29d4fe6fc48be7334aaa8
sicilyChen <chenss_96@163.com>
2021-09-17 19:39:38 +0800

    first exercise

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-hello
+file=open('d.txt','w') #文件的写入
+file.write('hello')
+file1=open('d.txt','a')#在文件中追加'world'
+file1.write('world')
+file1.flush()#把缓冲区的内容写入文件，但不关闭文件
+file1.write('python')
+file1.close
